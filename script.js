11908c42d5b79464cceb0f1ce769c6bca7265874
Kartik Mittal <147180780+kartikmittal19@users.noreply.github.com>
2024-06-27 09:28:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let display=document.querySelector("#display")
+function sin(){
+  display.value=Math.sin(display.value)
+}
+function cos(){
+  display.value=Math.cos(display.value)
+}
+function tan(){
+  display.value=Math.tan(display.value)
+}
+function sqrt(){
+  display.value=Math.sqrt(display.value)
+}
